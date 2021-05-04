79d21c223c020ecc35ade4ba1ce112c7dac7cb3f
Donghyeon Cho <hyeon9698@naver.com>
2021-05-05 02:56:36 +0900

    [update] check.py

--- check.py
+++ check.py
@@ -5,6 +5,7 @@ from pytz import timezone
 import telegram
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 # 필요 API KEY load
 load_dotenv()
@@ -14,11 +15,11 @@ upbit = pyupbit.Upbit(access, secret)
 token = os.getenv('TELEGRAM_API_TOKEN_KEY')
 mc = os.getenv('TELEGRAM_API_MC')
 bot = telegram.Bot(token)
+df = pd.read_csv('check_dataset.csv')
 
 # 필요 변수 초기화
-coin_list = ["KRW-ENJ", "KRW-SAND", "KRW-DOGE", "KRW-BTT", "KRW-XRP", "KRW-DKA", "KRW-MED", "KRW-BTC", "KRW-ADA", "KRW-ETH"]
+coin_list = ["KRW-BTC", "KRW-XRP"]
 n = len(coin_list)
-prices_open = [0]*(n)
 prices_prev = [0]*(n)
 prices_now = [0]*(n)
 prices_high_5 = [0]*(n)
@@ -31,28 +32,34 @@ save_low = True
 now = datetime.now(timezone('Asia/Seoul'))
 prev_day = now.day
 
-# 실행하면 바로 업데이트
+# 실행하면 바로 업데이트 
+# 주석 처리 하기
 for i in range(n):
-    prices_open[i] = pyupbit.get_ohlcv(coin_list[i], "day").iloc[-1]['open']
-    print(prices_open[i], coin_list[i])
     prices_prev[i] = pyupbit.get_current_price(coin_list[i])
     prices_high_5[i] = prices_prev[i] + prices_prev[i] * 0.05
-    prices_high_15[i] = prices_open[i] + prices_open[i] * 0.15
+    prices_high_15[i] = prices_prev[i] + prices_prev[i] * 0.15
     prices_low_5[i] = prices_prev[i] - prices_prev[i] * 0.05
-    prices_low_15[i] = prices_open[i] - prices_open[i] * 0.15
+    prices_low_15[i] = prices_prev[i] - prices_prev[i] * 0.15
+    
+    df.loc[i,'prices_prev'] = prices_prev[i]
+    df.loc[i,'prices_low_5'] = prices_low_5[i]
+    df.loc[i,'prices_low_15'] = prices_low_15[i]
+    df.loc[i,'prices_high_5'] = prices_high_5[i]
+    df.loc[i,'prices_high_15'] = prices_high_15[i]
     time.sleep(0.1)
+df.to_csv('check_dataset.csv', index=None)
+
+# 기존 데이터 가지고 오기
+for i in range(n):
+    prices_prev[i] = df.loc[i,'prices_prev']
+    prices_high_5[i] = df.loc[i,'prices_high_5']
+    prices_high_15[i] = df.loc[i,'prices_high_15']
+    prices_low_5[i] = df.loc[i,'prices_low_5']
+    prices_low_15[i] = df.loc[i,'prices_low_15']
 
 while True:
     # 지금 한국 시간
     now = datetime.now(timezone('Asia/Seoul'))
-        
-    # 하루에 한번 작동하는 save
-    if prev_day != now.day:
-        prev_day = now.day
-        save = True
-        msg = "save 변수가 True로 업데이트 됐습니다.\nsave: " + str(save)
-        print(msg)
-        bot.sendMessage(mc,msg)
     
     # 매 시간 가격
     msg = f'----------{now.strftime("%Y-%m-%d %H:%M:%S")}----------\n'
@@ -61,19 +68,6 @@ while True:
         msg += f'{"%10s"%coin_list[i]} {prices_now[i]}원\n'
         time.sleep(0.1)
     print(msg)
-
-    # 아침 9시에 prices_open 변수 새로 설정
-    if now.hour == 9 and now.minute == 0 and now.second > 30 and save:
-        save = False
-        msg = "시가가 바꼈습니다.\n"
-        for i in range(n):
-            prices_open[i] = pyupbit.get_ohlcv(coin_list[i], "day").iloc[-1]['open']
-            prices_high_15[i] = prices_open[i] + prices_open[i] * 0.15
-            prices_low_15[i] = prices_open[i] - prices_open[i] * 0.15
-            msg += f'{"4%s"%coin_list[i]} -> {prices_open[i]}원\n'
-            time.sleep(0.1)
-        print(msg)
-        bot.sendMessage(mc,msg)
 
     # where the magic happens - main code
     for i in range(n):
@@ -85,7 +79,7 @@ while True:
             prices_high_5[i] = prices_now[i] + prices_now[i] * 0.05
             prices_low_5[i] = prices_now[i] - prices_now[i] * 0.05
         if prices_now[i] <= prices_low_5[i]:
-            msg = f'{coin_list[i]} {prices_prev[i]}원 -> {prices_now[i]}원 됨. \n5프로 내려감 확인 바람\n'
+            msg = f'@@@{coin_list[i]}@@@ {prices_prev[i]}원 -> {prices_now[i]}원 됨. \n5프로 내려감 확인 바람\n'
             print(msg)
             bot.sendMessage(mc,msg)
             prices_prev[i] = prices_now[i]
@@ -101,4 +95,4 @@ while True:
             msg = f'!!!!!!!!!!{coin_list[i]} {prices_now[i]}원\n시가 대비 15프로나 떨어졌음!!!!!!!!!!\n'
             print(msg)
             bot.sendMessage(mc,msg)
-    time.sleep(1)
+    time.sleep(10) # 너무 자주 안 돌려도 된다고 생각해서 10초 딜레이 추가함
